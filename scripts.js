89704deb7a8c0291187d3edce79c6e630054b0d4
Simanga <simacoder@hotmail.com>
2024-04-29 20:03:51 +0200

    add few fixes

--- scripts.js
+++ scripts.js
@@ -1,16 +1,46 @@
-var emailField = document.getElementById('email-field');
-var emailError = document.getElementById('email-error');
+const email = document.getElementById('email');
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-function validateEmail(){
+    validateInputs();
+});
 
-    if(!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML = "Please provide a valid email";
-        return false;
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+
+    const emailValue = email.value.trim();
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Please provide a valid email ');
+    } else {
+        setSuccess(email);
     }
 
-    emailError.innerHTML = "";
-    return true;
-}
+    
+
+};
 
